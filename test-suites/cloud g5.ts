306cd820eaab3a45e9a56e5d05ccdd1ddeb151ea
tuyenKatalon <110393035+tuyenKatalon@users.noreply.github.com>
2023-05-29 15:19:05 +0700

    init

---
+++ test-suites/cloud g5.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>cloud g5</name>
+    <testSuiteGuid>c35f6363-f61c-402e-82c8-e49271963df5</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/g5 pass</testCaseId>
+        <guid>aa9bb649-3b83-401a-b017-f353d2453a62</guid>
+    </testCaseLink>
+</TestSuiteEntity>
